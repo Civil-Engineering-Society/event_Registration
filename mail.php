7cc00e18257929943199b0aab33fc25c520bdb1f
Jerin Jacob <82531317+jerinja@users.noreply.github.com>
2022-04-10 20:35:12 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "youremail@mail.com";
+$to = "jerinjacob0481@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
